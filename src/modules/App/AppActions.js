a6f2e7a0bca0074116e529a59059a84972d998b8
blockchainpartner <services@blockchainpartner.fr>
2018-11-20 14:23:42 +0000

    added web3 with MM on the app

--- src/modules/App/AppActions.js
+++ src/modules/App/AppActions.js
@@ -24,7 +24,29 @@ export function errorApp(statusCode, type, title, message) {
 
 
 export function setWeb3() {
-    return (dispatch) => {
-        dispatch(eventWeb3("hallo", "haaaalloooooo"));
+    return async (dispatch) => {
+        let web3;
+        if (window.ethereum) {
+            web3 = new Web3(window.ethereum);
+            try {
+                // Request account access if needed
+                await window.ethereum.enable();
+                // Acccounts now exposed
+            } catch (error) {
+                // User denied account access...
+                dispatch(errorApp(400, "denied", "Denied access", "MetaMask access was denied by the user"));
+            }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+            web3 = new Web3(window.web3.currentProvider);
+            // Acccounts always exposed
+        }
+        // Non-dapp browsers...
+        else {
+            dispatch(errorApp(400, "notFound", "MetaMask not found", "MetaMask is not install on this browser"));
+        }
+
+        dispatch(eventWeb3(web3, "a"));
     }
 }
